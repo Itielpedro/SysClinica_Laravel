25e418a0c3586b8cabe0fe04b52fd1700ebab0ad
Itielpedro <itielpedro2014@gmail.com>
2023-11-24 11:44:21 -0300

    Update Relatorios

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\DB;
 
 class PDF extends TCPDF
 {
-    // ... Seu código existente ...
-
     public function Header()
     {
 
@@ -24,18 +22,15 @@ class PDF extends TCPDF
             $imageFile = public_path('images/tcpdf_logo.jpg');
             $this->Image($imageFile, 10, 10, 20, '', 'JPG', '', 'T', false, 100, '', false, false, 0, false, false, false);
 
-            // Posicionar o título ao centro
             $this->SetFont('helvetica', 'B', 20);
-            $this->SetXY(20, 15); // Ajuste a posição conforme necessário
+            $this->SetXY(20, 15);
             $this->Cell(0, 10, 'MEDICAL GROUP', 0, 1, 'C');
 
-            // Definir a posição da linha horizontal abaixo da logo
             $this->SetLineStyle(array('width' => 0.5, 'color' => array(0, 0, 0)));
             $this->Line(10, $this->GetY() + 10, $this->getPageWidth() - 10, $this->GetY() + 10);
         }
 
-        // Defina a posição vertical do cursor após o cabeçalho
-        $this->SetY($this->GetY() + 20);
+        $this->SetY($this->GetY() + 10);
     }
 
     public function Footer()
@@ -43,15 +38,12 @@ class PDF extends TCPDF
         $this->SetY(-25);
         $this->SetLineStyle(array('width' => 0.5, 'color' => array(0, 0, 0)));
         $this->Line(10, $this->GetY(), $this->getPageWidth() - 10, $this->GetY());
-        // Adicionar o texto de copyright no rodapé
+
         $this->SetFont('helvetica', 'I', 12);
         $this->SetY(-20);
         $this->Cell(0, 0, '© 2023 MEDICAL GROUP. Todos os direitos reservados.', 0, 0, 'C');
     }
 }
-
-
-
 
 class RelatorioController extends Controller
 {
@@ -64,8 +56,6 @@ class RelatorioController extends Controller
         $data = \Carbon\Carbon::parse($paciente->data_nasc)->format('d/m/Y');
 
         $pdf = new PDF();
-
-
 
         $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
@@ -83,52 +73,51 @@ class RelatorioController extends Controller
         $html = <<<EOD
         <table border="1" cellspacing="5" cellpadding="2">
 
-
         <h3 style="text-align:center;">INFORMAÇÕES PESSOAIS</h3>
 
-        <tr  style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th >NOME</th>
             <td> $paciente->nome</td>
         </tr>
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>CPF</th>
             <td>$paciente->cpf</td>
         </tr>
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>RG</th>
             <td>$paciente->rg</td>
         </tr>
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>Data de Nascimento</th>
             <td> $data</td>
         </tr>
 
         <h3 style="text-align:center;">ENDEREÇO</h3>
 
-        <tr style="background-color:#128bc8;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>RUA</th>
             <td>$paciente->rua</td>
         </tr>
-        <tr style="background-color:#128bc8;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>NUMÉRO</th>
             <td>$paciente->numero</td>
         </tr>
-        <tr style="background-color:#128bc8;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>BAIRRO</th>
             <td>$paciente->bairro</td>
         </tr>
-        <tr style="background-color:#128bc8;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>CIDADE</th>
-            <td> $paciente->cidade</td>
+            <td> $paciente->cidade - $paciente->estado</td>
         </tr>
 
         <h3 style="text-align:center;">CONTATO</h3>
 
-        <tr>
+        <tr  style="background-color:#dfe6e9;">
             <th>TELEFONE</th>
             <td> $paciente->telefone</td>
         </tr>
-        <tr>
+        <tr  style="background-color:#dfe6e9;">
             <th>EMAIL</th>
             <td>$paciente->email </td>
         </tr>
@@ -154,26 +143,26 @@ EOD;
         $pdf->setFontSize(15);
         $pdf->setFont('', '');
 
-        $html = ''; // Inicializar a variável HTML
+        $html = '';
 
         foreach ($aniversariantes as $aniversariante) {
             $data = \Carbon\Carbon::parse($aniversariante->data_nasc)->format('d/m/Y');
 
             $html .= <<<EOD
             <table border="1" cellspacing="5" cellpadding="2">
-                <tr>
-                    <th >NOME</th>
+                <tr style="background-color:#dfe6e9;">
+                    <th>NOME</th>
                     <td> $aniversariante->nome</td>
                 </tr>
-                <tr >
+                <tr style="background-color:#dfe6e9;">
                     <th>TELEFONE</th>
                     <td>$aniversariante->telefone</td>
                 </tr>
-                <tr>
+                <tr style="background-color:#dfe6e9;">
                     <th>EMAIL</th>
                     <td>$aniversariante->email</td>
                 </tr>
-                <tr>
+                <tr style="background-color:#dfe6e9;">
                     <th>Data de Nascimento</th>
                     <td> $data</td>
                 </tr>
@@ -197,8 +186,6 @@ EOD;
 
         $pdf = new PDF();
 
-
-
         $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
         $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
@@ -215,63 +202,61 @@ EOD;
         $html = <<<EOD
         <table border="1" cellspacing="5" cellpadding="2" style="">
 
-
         <h3 style="text-align:center;">INFORMAÇÕES PESSOAIS</h3>
 
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th >NOME</th>
             <td> $funcionario->nome</td>
         </tr>
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>CPF</th>
             <td>$funcionario->cpf</td>
         </tr>
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>RG</th>
             <td>$funcionario->rg</td>
         </tr>
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>DATA DE NASCIMENTO</th>
             <td>$data</td>
         </tr>
 
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>CARGO</th>
             <td>$funcionario->cargo</td>
         </tr>
 
-        <tr style="background-color:green;color:white;">
+        <tr style="background-color:#dfe6e9;">
             <th>DATA DE ADMISSÃO</th>
             <td>$data_admissao</td>
         </tr>
 
-
         <h3 style="text-align:center;">ENDEREÇO</h3>
 
-        <tr>
+        <tr style="background-color:#dfe6e9;">
             <th>RUA</th>
             <td>$funcionario->rua</td>
         </tr>
-        <tr>
+        <tr style="background-color:#dfe6e9;">
             <th>NUMÉRO</th>
             <td>$funcionario->numero</td>
         </tr>
-        <tr>
+        <tr style="background-color:#dfe6e9;">
             <th>BAIRRO</th>
             <td>$funcionario->bairro</td>
         </tr>
-        <tr>
+        <tr style="background-color:#dfe6e9;">
             <th>CIDADE</th>
             <td> $funcionario->cidade</td>
         </tr>
 
         <h3 style="text-align:center;">CONTATO</h3>
 
-        <tr>
+        <tr style="background-color:#dfe6e9;">
             <th>TELEFONE</th>
             <td> $funcionario->telefone</td>
         </tr>
-        <tr>
+        <tr style="background-color:#dfe6e9;">
             <th>EMAIL</th>
             <td>$funcionario->email </td>
         </tr>
@@ -306,23 +291,24 @@ EOD;
         $html = <<<EOD
         <table border="1" cellspacing="5" cellpadding="2">
             <h3 style="text-align:center;">INFORMAÇÕES PESSOAIS</h3>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>DATA DE CADASTRO</th>
                 <td> $data_cadastro </td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>NOME</th>
                 <td> {$prontuario->paciente->nome} </td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>CPF</th>
                 <td>{$prontuario->paciente->cpf}</td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>RG</th>
                 <td>{$prontuario->paciente->rg}</td>
             </tr>
         </table>
+        <p></p>
         <h3 style="text-align:center;">LISTA DE CONSULTAS</h3>
         EOD;
 
@@ -332,19 +318,21 @@ EOD;
 
             $html .= <<<EOD
             <table border="1" cellspacing="5" cellpadding="2">
-                <tr style="background-color:green;color:white;">
+                <tr style="background-color:#dfe6e9;">
                     <th>DATA DA CONSULTA</th>
                     <td> $data_consulta </td>
                 </tr>
-                <tr style="background-color:green;color:white;">
+                <tr style="background-color:#dfe6e9;">
                     <th>HORA DA CONSULTA</th>
                     <td> $hora_consulta </td>
                 </tr>
-                <tr style="background-color:green;color:white;">
+                <tr style="background-color:#dfe6e9;">
                     <th>MÉDICO</th>
                     <td>{$consulta->medico->nome} </td>
                 </tr>
             </table>
+            <br>
+            <br>
             EOD;
         }
 
@@ -358,10 +346,8 @@ EOD;
         $especialidadeId = $request->input('especialidade_id');
         $medicoId = $request->input('medico_id');
 
-        // Construa a consulta base
         $agendamento = Agendamento::query();
 
-        // Adicione as condições conforme necessário
         if (!empty($dataEscolhida)) {
             $agendamento->whereDate('data', $dataEscolhida);
         }
@@ -376,10 +362,8 @@ EOD;
             $agendamento->where('medico_id', $medicoId);
         }
 
-        // Execute a consulta
         $consultas = $agendamento->with(['paciente', 'medico', 'especialidade'])->get();
 
-        // Crie um PDF com os dados das consultas
         $pdf = new PDF();
         $pdf->SetAutoPageBreak(true, 30);
         $pdf->AddPage();
@@ -392,12 +376,10 @@ EOD;
         $pdf->setFont('', '');
 
         if ($consultas->isEmpty()) {
-            // Adicione uma mensagem se não houver consultas agendadas
-            $html = '<p style="text-align:center">Não há consultas agendadas para o dia escolhido.</p>';
+            $html = '<p style="text-align:center">Não há consultas agendadas para o dia, especialidade ou médico escolhidos.</p>';
             $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
         } else {
             foreach ($consultas as $consulta) {
-                // Se necessário, ajuste a posição Y conforme necessário antes de cada bloco de tabela
                 $pdf->SetY($pdf->GetY() + 3);
 
                 $data_consulta = \Carbon\Carbon::parse($consulta->data)->format('d/m/Y');
@@ -405,31 +387,31 @@ EOD;
 
                 $html = <<<EOD
                 <table border="1" cellspacing="5" cellpadding="2">
-                    <tr>
+                    <tr style="background-color:#dfe6e9;">
                         <th >DATA</th>
                         <td> $data_consulta</td>
                     </tr>
-                    <tr >
+                    <tr style="background-color:#dfe6e9;">
                         <th>HORA</th>
                         <td>$hora_consulta</td>
                     </tr>
-                    <tr>
+                    <tr style="background-color:#dfe6e9;">
                         <th>PACIENTE</th>
                         <td>{$consulta->paciente->nome}</td>
                     </tr>
-                    <tr>
+                    <tr style="background-color:#dfe6e9;">
                         <th>TELEFONE DO PACIENTE</th>
                         <td>{$consulta->paciente->telefone}</td>
                     </tr>
-                    <tr>
+                    <tr style="background-color:#dfe6e9;">
                         <th>MÉDICO</th>
                         <td>{$consulta->medico->nome}</td>
                     </tr>
-                    <tr>
+                    <tr style="background-color:#dfe6e9;">
                         <th>ESPECIALIDADE</th>
                         <td>{$consulta->medico->especialidade->nome}</td>
                     </tr>
-                    <tr>
+                    <tr style="background-color:#dfe6e9;">
                         <th>RETORNO</th>
                         <td>$consulta->retorno</td>
                     </tr>
@@ -446,9 +428,7 @@ EOD;
 
     public function reciboConsulta($id)
     {
-
         $atendimento = Consulta::with(['atendimentos.procedimento'])->find($id);
-
 
         $data_consulta = \Carbon\Carbon::parse($atendimento->data)->format('d/m/Y');
         $hora_consulta = \Carbon\Carbon::parse($atendimento->hora)->format('H:i:s');
@@ -468,35 +448,35 @@ EOD;
 
         $html = <<<EOD
         <table border="1" cellspacing="5" cellpadding="2">
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>COD. DA CONSULTA</th>
                 <td>{$atendimento->id}</td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>DATA DA CONSULTA</th>
                 <td> $data_consulta </td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>HORA DA CONSULTA</th>
                 <td> $hora_consulta </td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>NOME DO PACIENTE</th>
                 <td>{$atendimento->paciente->nome}</td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>CPF</th>
                 <td>{$atendimento->paciente->cpf}</td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>NOME DO MÉDICO</th>
                 <td>{$atendimento->medico->nome}</td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>ESPECIALIDADE</th>
                 <td>{$atendimento->medico->especialidade->nome}</td>
             </tr>
-            <tr style="background-color:green;color:white;">
+            <tr style="background-color:#dfe6e9;">
                 <th>VALOR DA CONSULTA</th>
                 <td>R$ {$atendimento->atendimentos->first()->procedimento->valor}</td>
 
@@ -519,13 +499,15 @@ EOD;
             ->join('procedimentos', 'atendimentos.procedimento_id', '=', 'procedimentos.id')
             ->sum('procedimentos.valor');
 
-
-        // Consulta para obter o total financeiro recebido dos demais procedimentos no período
         $totalOutrosProcedimentos = Atendimento::whereBetween('consultas.data', [$dataInicio, $dataFim])
             ->where('procedimentos.descricao', '<>', 'consulta')
             ->join('consultas', 'atendimentos.consulta_id', '=', 'consultas.id')
             ->join('procedimentos', 'atendimentos.procedimento_id', '=', 'procedimentos.id')
             ->sum('procedimentos.valor');
+
+        $totalGeral = $totalConsultas + $totalOutrosProcedimentos;
+        $totalGeralFormatado = number_format($totalGeral, 2, '.', '');
+
 
 
         $consultasPorEspecialidade = Atendimento::whereBetween('consultas.data', [$dataInicio, $dataFim])
@@ -536,7 +518,7 @@ EOD;
             ->select(
                 'especialidades.nome as especialidade',
                 DB::raw('count(*) as total_consultas'),
-                DB::raw('sum(procedimentos.valor) as total_valor') // Adiciona o valor total por especialidade
+                DB::raw('sum(procedimentos.valor) as total_valor')
             )
             ->groupBy('especialidades.nome')
             ->get();
@@ -549,27 +531,58 @@ EOD;
         $pdf->SetFontSize(20);
         $pdf->SetFont('', 'B');
         $pdf->Cell(0, 20, 'Relatório Financeiro', 0, 1, 'C');
+
         $pdf->SetFontSize(14);
         $pdf->SetFont('', '');
 
-        // Adicionar informações ao PDF
-        $pdf->Cell(0, 10, 'Total Consultas: R$ ' . $totalConsultas, 0, 1);
-        $pdf->Cell(0, 10, 'Total Outros Procedimentos: R$ ' . $totalOutrosProcedimentos, 0, 1);
-        $pdf->Cell(0, 10, 'Consultas por Especialidade', 0, 1);
+        $data1 = \Carbon\Carbon::parse($dataInicio)->format('d/m/Y');
+        $data2 = \Carbon\Carbon::parse($dataFim)->format('d/m/Y');
 
-        // Adicionar tabela de consultas por especialidade
-        $pdf->SetFont('', 'B');
-        $pdf->Cell(60, 10, 'Especialidade', 1, 0, 'C');
-        $pdf->Cell(60, 10, 'Total Consultas', 1, 0, 'C');
-        $pdf->Cell(60, 10, 'Total Valor Recebido', 1, 1, 'C');
-        $pdf->SetFont('', '');
+        $html = <<<EOD
+        <p><i>Período de {$data1} - {$data2}</i></p>
+        <table border="1" cellspacing="5" cellpadding="2">
+        <h3 style="text-align:center;padding-top:;">CONSULTAS</h3>
+
+            <tr style="background-color:#dfe6e9;">
+                <th>TOTAL DE CONSULTAS</th>
+                <td>R$ $totalConsultas</td>
+            </tr>
+            <tr style="background-color:#dfe6e9;">
+                <th>TOTAL PROCEDIMENTOS</th>
+                <td>R$ $totalOutrosProcedimentos</td>
+            </tr>
+            <tr style="background-color:#dfe6e9;">
+                <th>TOTAL GERAL</th>
+                <td>R$ $totalGeralFormatado</td>
+            </tr>
+        </table>
+        <p></p>
+        <h3 style="text-align:center; margin-top: 20px;">ESPECIALIDADES</h3>
+        EOD;
+
 
         foreach ($consultasPorEspecialidade as $especialidade) {
-            $pdf->Cell(60, 10, $especialidade->especialidade, 1, 0);
-            $pdf->Cell(60, 10, $especialidade->total_consultas, 1, 0, 'C');
-            $pdf->Cell(60, 10, 'R$ ' . $especialidade->total_valor, 1, 1, 'C');
-        }
+            $html .= <<<EOD
 
+            <table border="1" cellspacing="5" cellpadding="2">
+                <tr style="background-color:#dfe6e9;">
+                    <th>ESPECIALIDADE</th>
+                    <td>$especialidade->especialidade</td>
+                </tr>
+                <tr style="background-color:#dfe6e9;">
+                    <th>TOTAL</th>
+                    <td>$especialidade->total_consultas</td>
+                </tr>
+                <tr style="background-color:#dfe6e9;">
+                    <th>VALOR</th>
+                    <td> R$ $especialidade->total_valor</td>
+                </tr>
+            </table>
+            <br>
+            <br>
+            EOD;
+        }
+        $pdf->writeHTMLCell(0, 0, '', '', $html);
         $pdf->Output('relatorio_financeiro.pdf', 'I');
     }
 }
